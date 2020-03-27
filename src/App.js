482ccc9dcd1f9ed6b1ed1a42b31aa043fd398e5a
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-03-27 17:07:37 +0600

    react rechart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SimpleLineChart from './Components/LineChart/SimpleLineChart';
+import SimpleBarChart from './Components/BarChart/SimpleBarChart';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Recharts Examples</h1>
+      <div className="chart1">
+        <SimpleLineChart />
+
+      </div>
+      <div className="chart2">
+        <SimpleBarChart />
+      </div>
     </div>
   );
 }
