23510733036e68788eac3977e76466879677817b
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-03-27 20:53:13 +0600

    Appbar using Material UI

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import SimpleLineChart from './Components/LineChart/SimpleLineChart';
 import SimpleBarChart from './Components/BarChart/SimpleBarChart';
+import PrimarySearchAppBar from './Components/MaterialUI/MaterialUI';
 
 
 function App() {
 
   return (
     <div className="App">
+      <PrimarySearchAppBar />
       <h1>React Recharts Examples</h1>
       <div className="chart1">
         <SimpleLineChart />
